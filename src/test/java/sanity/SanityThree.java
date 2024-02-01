90c04d2debd724afb677677ea16cd795b0914e79
pawarkavi12 <pawarkavi12@gmail.com>
2024-02-01 14:55:21 +0530

    New sanity three class is added by new user

---
+++ src/test/java/sanity/SanityThree.java
@@ -0,0 +1,22 @@
+package sanity;
+
+import org.testng.annotations.Test;
+
+public class SanityThree {
+	
+	@Test
+	void n1()
+	{
+		System.out.println("Run n1() method");
+	}
+	@Test
+	void n2()
+	{
+		System.out.println("Run n2() method");
+	}
+	@Test  
+	void n3()
+	{
+		System.out.println("Run n3() method");
+	}
+}
